a4d521463e2c8ddf8f5b19e7946e107ab64739bc
Ray-tech178 <67976062+Ray-tech178@users.noreply.github.com>
2020-07-15 13:52:05 -0500

    Add files via upload

---
+++ Raymond_L_CollectionExercise.playground/Contents.swift
@@ -0,0 +1,4 @@
+import UIKit
+
+var games : [String] = ["Stardew Valley","Terraria", "Battlefield V", "Grand Theft Auto V", "Killing Floor 2", "Astroneer", "Dying Light", "Ark Survival Evolved", "Slime Rancher", "Minecraft"]
+let totalItem = games.count
